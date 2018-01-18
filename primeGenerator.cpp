e11a1a9352170c0f3f6d6d200d9e25d4bf381c75
Gabriel Nowak <gbrl.nowak@gmail.com>
2018-01-18 13:16:40 +0100

    Update primeGenerator.cpp

--- primeGenerator.cpp
+++ primeGenerator.cpp
@@ -5,17 +5,17 @@ Help him! Your task is to generate all prime numbers between two given numbers
 */
 #include<iostream>
 using namespace std;
-bool isPrime(int x)
+bool isPrime(int x)// return true if parameter is prime number
 {
     if(x==1)return false;
     if(x==2 or x==3)return true;
-    for(int i=2;i*i<=x;i++)
+    for(int i=2;i*i<=x;i++)// the algorithm ends when we will come to i=sqrt(x)
     {
-        if(x%i==0) return false;
+        if(x%i==0) return false;// if we found even one divider then false
     }
     return true;
 }
-void findPrime(int a, int b)
+void findPrime(int a, int b)// finding and printing all prime numbers in the range [a,b] by using previous function isPrime
 {
     for(int i=a;i<=b;i++)
     {
