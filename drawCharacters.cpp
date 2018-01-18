2796b63bf62ebbab05025adfda33ea8f490e0d3e
Gabriel Nowak <gbrl.nowak@gmail.com>
2018-01-18 13:07:58 +0100

    Update drawCharacters.cpp

--- drawCharacters.cpp
+++ drawCharacters.cpp
@@ -12,15 +12,15 @@ using namespace std;
 int main()
 {
     int t;
-    cin>>t;
-    string x;
+    cin>>t;//ammount of tests
+    string x;//text on we are working 
     for(int i=0;i<t;i++)
     {
         cin>>x;
-        if(x.length()/2>=1 and x.length()/2<=100)
+        if(x.length()/2>=1 and x.length()/2<=100)// 1<=k<=100 - exercice's condition
         {
-            for(int j=0;j<x.length()/2;j+=2) cout<<x[j];
-            cout<<"\n";
+            for(int j=0;j<x.length()/2;j+=2) cout<<x[j];//we are iterating only every second letter until half of the string
+            cout<<"\n";// every new test in the new line
         }
     }
     return 0;
